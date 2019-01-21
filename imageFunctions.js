128e09befd2c7764270dd4031f8c7bed6b4e6a1d
Christopher Hoffman <chr1snv@gmail.com>
2019-01-21 11:50:57 -0800

    working on local maxima feature detection filtering

--- imageFunctions.js
+++ imageFunctions.js
@@ -2,11 +2,62 @@
 
 class Feature
 {
+
+        locationString()
+        {
+                return this.x+":"+this.y;
+        }
+
+        lookUpAndAddNeighborLink(dx,dy)
+        {
+                str = this.x+dx+":"+this.y+dy;
+        }
         constructor(xin, yin, variancein)
         {
-             this.x = xin;
-             this.y = yin;
-             this.variance = variancein;
+                this.x = xin;
+                this.y = yin;
+                this.variance = variancein;
+
+                //append links to all of the neighboring features for later local maxima finding
+
+                //f00 f01 f02
+                //f10 f11 f12
+                //f20 f21 f22
+
+                this.neighboringFeatures = {};
+                //top row
+                var f00 = featureDict[this.x-1+":"+this.y-1];
+                if( f00 != undefined )
+                {
+                        this.neighboringFeatures[f00.locationString()] = f00;
+                        f00.neighboringFeatures[this.locationString()] = this;
+                }
+                var f01 = featureDict[this.x-0+":"+this.y-1];
+                if( f01 != undefined )
+                        this.neighboringFeatures.push( f01 );
+                var f02 = featureDict[this.x+1+":"+this.y-1];
+                if( f02 != undefined )
+                        this.neighboringFeatures.push( f02 );
+                //middle row
+                var f10 = featureDict[this.x-1+":"+this.y-0];
+                if( f02 != undefined )
+                        this.neighboringFeatures.push( f10 );
+                var f11 = featureDict[this.x-0+":"+this.y-0];
+                if( f02 != undefined )
+                        this.neighboringFeatures.push( f11 );
+                var f12 = featureDict[this.x+1+":"+this.y-0];
+                if( f12 != undefined )
+                        this.neighboringFeatures.push( f12 );
+                //bottom row
+                var f20 = featureDict[this.x-1+":"+this.y+1];
+                if( f20 != undefined )
+                        this.neighboringFeatures.push( f20 );
+                var f21 = featureDict[this.x-0+":"+this.y+1];
+                if( f21 != undefined )
+                        this.neighboringFeatures.push( f21 );
+                var f22 = featureDict[this.x+1+":"+this.y+1];
+                if( f22 != undefined )
+                        this.neighboringFeatures.push( f22 );
         }
 
 }
@@ -37,14 +88,25 @@ function DetectFeatures(imgd)
 
                         if(variance > threshold )
                         {
+                                //set the pixel black to indicate where the feature is
                                 pix[i+0] = 0;
                                 pix[i+1] = 0;
                                 pix[i+2] = 0;
-                                newFeature = new Feature(iw,ih,variance);
+                                newFeature = new Feature(iw,ih,variance, featureDict);
                                 featureDict[iw+":"+ih] = newFeature;
+
+                                
+
+
                         }
 
                 }
         }
         return featureDict;
+}
+
+
+function findLocalMaximalFeatures(featureDict)
+{
+        
 }
